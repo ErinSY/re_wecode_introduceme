6d7c892062e93e63a2d999d76218fa9d7ec22676
ErinSY <99514580+ErinSY@users.noreply.github.com>
2022-06-23 23:50:59 +0900

    add dice game

--- about.js
+++ about.js
@@ -19,4 +19,41 @@ function typing() {
   }
 }
 
+
 setInterval(typing,200)
+
+
+function game () {
+  var randomNumber1 = 
+    Math.floor(Math.random() * 6) + 1;
+
+var randomImage ="images/" + "dice"+randomNumber1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImage)
+
+
+
+var randomNumber2 = 
+    Math.floor(Math.random() * 6) + 1;
+
+var randomImage2 ="images/" + "dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2)
+
+
+if (randomNumber1>randomNumber2) {
+
+    document.getElementById('title').innerHTML = "Player 1 Win!"
+   
+}
+else if (randomNumber1<randomNumber2) {
+    document.getElementById('title').innerHTML = "Player 2 Win!"
+
+}
+else{
+    document.getElementById('title').innerHTML = "DRAW"  
+}
+
+
+
+}
