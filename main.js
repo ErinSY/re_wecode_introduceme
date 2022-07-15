79d74f3653bb9335e2ae09abfb531e3328a09da4
하서율 <seoyul0203@gmail.com>
2022-07-15 15:09:16 +0900

    현재시간 추가

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ function changeColor() {
 }
 
 button.addEventListener("click", changeColor);
+
+function currentTime () {
+  const now = new Date();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+  const ptag = document.querySelector('p');
+  ptag.innerHTML = `${hour} : ${min} : ${sec}`;
+}
+
+function livetime() {
+currentTime();
+ setInterval(currentTime,1000)
+
+}
+
+window.addEventListener('load', livetime)
